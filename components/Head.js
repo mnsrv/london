34e136de2e1323d5bbb28ea4b9271d24afee2929
Aleksandr Mansurov <mansurov@rocketguys.com>
2018-01-19 12:06:00 +0300

    add GA

--- components/Head.js
+++ components/Head.js
@@ -14,5 +14,14 @@ export default () => (
         background-color: #fafafa;
       }
     `}</style>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112751787-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-112751787-1');
+    </script>
   </Head>
 )
