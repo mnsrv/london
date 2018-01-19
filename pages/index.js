bbb3433abc66a8638f813d77b01660d588e19727
Aleksandr Mansurov <mansurov@rocketguys.com>
2018-01-19 11:39:52 +0300

    fix city id

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,12 @@ const Index = (props) => (
 )
 
 Index.getInitialProps = async function() {
-  const city = 'Moscow'
+  const city = '524901'
   if (!WEATHER_API_KEY) {
     return false
   }
   const appid = WEATHER_API_KEY
-  const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${appid}`)
+  const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?id=${city}&units=metric&APPID=${appid}`)
   const data = await res.json()
 
   return {
