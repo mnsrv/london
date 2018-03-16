d4a28e934592b24247046bc444e2fe2aa689889f
Sasha Mansurov <mansurov@rocketguys.com>
2018-03-16 17:34:58 +0300

    Add Chatra

--- components/Head.js
+++ components/Head.js
@@ -366,5 +366,20 @@ export default () => (
       gtag('js', new Date());
       gtag('config', 'UA-112751787-1');
     `}}></script>
+    <!-- Chatra {literal} -->
+    <script>
+        (function(d, w, c) {
+            w.ChatraID = 'aeSjcHXJXuKpCPZPA';
+            var s = d.createElement('script');
+            w[c] = w[c] || function() {
+                (w[c].q = w[c].q || []).push(arguments);
+            };
+            s.async = true;
+            s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+            + '//call.chatra.io/chatra.js';
+            if (d.head) d.head.appendChild(s);
+        })(document, window, 'Chatra');
+    </script>
+    <!-- /Chatra {/literal} -->
   </Head>
 )
