ed8cb8ab1fd5fbe94d72418a8f3ec9ccae4d011d
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-06-30 21:10:37 +0300

    Add knockout bracket

--- pages/worldcup.js
+++ pages/worldcup.js
@@ -52,88 +52,190 @@ export default class WorldCupPage extends React.Component {
     const groupMatches = getMatches(groups)
     const knockoutMatches = getMatches(knockout)
 
-    return { groups, groupMatches, knockoutMatches, stadiums, teams }
+    return { groups, groupMatches, knockout, knockoutMatches, stadiums, teams }
   }
 
   render() {
-    const { groups, groupMatches, stadiums, teams } = this.props
-    const remainingMatches = groupMatches.filter(item => new Date().getTime() <= Number(item[0]))
+    const { groups, knockout, stadiums, teams } = this.props
+
+    const KnockoutMatch = ({ match }) => {
+      const homeTeam = teams[match.home_team - 1] || {}
+      const awayTeam = teams[match.away_team - 1] || {}
+      const now = new Date()
+      const date = new Date(match.date)
+      const nowPlaying = now.getTime() - date.getTime() > 0 && !match.finished
+      const color = nowPlaying ? 'rgb(255,40,0)' : '#aaa'
+      const dateString = `${date.getDate()} ${localeMonthsGenitive[date.getMonth()]}`
+      const time = nowPlaying ? 'Идёт сейчас.' : `${dateString} ${date.getHours()}:${getFullMinutes(date)}`
+      const stadium = stadiums[match.stadium - 1]
+      const place = stadium.city === 'Moscow' ? `${localeCities[stadium.city]}. ${localeStadiums[stadium.name]}` : `${localeCities[stadium.city]}`
+      const homeScore = nowPlaying ? match.home_result || '0' : match.home_result || '\u2007'
+      const awayScore = nowPlaying ? match.away_result || '0' : match.away_result || '\u2007'
+      const className = nowPlaying ? 'knockout nowPlaying' : 'knockout'
+
+      return (
+        <div className={className}>
+          <div className="knockout__info"><span className="knockout__time">{time}</span><br />{place}</div>
+          <div className="knockout__team">
+            <div className="knockout__team-name">{homeTeam.emojiString} {localeCountries[homeTeam.name]}</div>
+            <div className="knockout__team-score">{homeScore}</div>
+          </div>
+          <div className="knockout__team">
+            <div className="knockout__team-name">{awayTeam.emojiString} {localeCountries[awayTeam.name]}</div>
+            <div className="knockout__team-score">{awayScore}</div>
+          </div>
+        </div>
+      )
+    }
+    const FinalMatch = ({ match, semi = false }) => {
+      const className = `knockout knockout_final ${semi ? 'knockout_semi' : ''}`
+      const homeTeam = teams[match.home_team - 1] || {}
+      const awayTeam = teams[match.away_team - 1] || {}
+      const now = new Date()
+      const date = new Date(match.date)
+      const nowPlaying = now.getTime() - date.getTime() > 0 && !match.finished
+      const dateString = `${date.getDate()} ${localeMonthsGenitive[date.getMonth()]}`
+      const time = nowPlaying ? 'Идёт сейчас.' : `${dateString} ${date.getHours()}:${getFullMinutes(date)}`
+      const stadium = stadiums[match.stadium - 1]
+      const place = stadium.city === 'Moscow' ? `${localeCities[stadium.city]}. ${localeStadiums[stadium.name]}` : `${localeCities[stadium.city]}`
+
+      return (
+        <div className={className}>
+          <div className="knockout__info">{time}<br />{place}</div>
+          <div className="knockout__team">
+            <div className="knockout__team-name">{homeTeam.emojiString} {localeCountries[homeTeam.name]}</div>
+            <div className="knockout__team-score">{match.home_result || '\u2007'}</div>
+          </div>
+          <div className="knockout__team">
+            <div className="knockout__team-score">{match.away_result || '\u2007'}</div>
+            <div className="knockout__team-name">{awayTeam.emojiString} {localeCountries[awayTeam.name]}</div>
+          </div>
+        </div>
+      )
+    }
+    const firstHalf = Object.entries(groups).filter((_, index) => index % 2 === 0).map(item => item[1].winner)
+    const secondHalf = Object.entries(groups).filter((_, index) => index % 2 === 1).map(item => item[1].winner)
+    const round16Matches = knockout.round_16.matches
+    const firstRound16Matches = round16Matches.filter(item => firstHalf.includes(item.home_team))
+    const secondRound16Matches = round16Matches.filter(item => secondHalf.includes(item.home_team))
+
+    const round8Matches = knockout.round_8.matches
+    const round8MatchesHalf = Math.floor(round8Matches.length / 2)
+    const firstRound8Matches = round8Matches.slice(0, round8MatchesHalf)
+    const secondRound8Matches = round8Matches.slice(round8MatchesHalf, round8Matches.length)
+
+    const round4Matches = knockout.round_4.matches
+    const round4MatchesHalf = Math.floor(round4Matches.length / 2)
+    const firstRound4Matches = round4Matches.slice(0, round4MatchesHalf)
+    const secondRound4Matches = round4Matches.slice(round4MatchesHalf, round4Matches.length)
 
     return (
       <Layout>
         <article>
           <section>
-            <table className="table_nowrap">
-              <thead>
-                <tr>
-                  <th colSpan={6} />
-                  {remainingMatches.map((item) => {
-                    const date = new Date(Number(item[0]))
-                    const dateString = `${date.getDate()} ${localeMonthsGenitive[date.getMonth()]}`
-                    return <th key={dateString}>{dateString}</th>
-                  })}
-                </tr>
-              </thead>
-              {Object.entries(groups).map((group, index, array) => {
-                const groupTeams = this.getGroupTeams(group)
-                return (
-                  <tbody key={group[0]}>
-                    {groupTeams.map(((item, index) => {
-                      const team = teams[item.id - 1]
-                      return (
-                        <tr key={item.id}>
-                          <td>{index === 0 ? group[0].toUpperCase() : ''}</td>
-                          <td>{team.emojiString} {localeCountries[team.name]}</td>
-                          <td>{item.points}</td>
-                          {item.matches.map((match) => {
-                            if (!match.finished) {
-                              return <td key={match.name} />
-                            }
-                            const a = match.home_team === item.id ? match.home_result : match.away_result
-                            const b = match.home_team === item.id ? match.away_result : match.home_result
-                            const competitor = match.home_team === item.id ? match.away_team : match.home_team
-                            const backgroundColor = this.getBackgroundColor(a, b)
-                            return <td key={match.name} style={{ backgroundColor }}>{a} – {b} {teams[competitor - 1].emojiString}</td>
-                          })}
-                          {remainingMatches.map((remainingMatch) => {
-                            const match = item.matches.find(match => remainingMatch[1].map(item => item.name).includes(match.name))
-                            if (!match || match.finished) {
-                              return <td key={remainingMatch[0]} />
-                            }
-                            const competitor = match.home_team === item.id ? match.away_team : match.home_team
-                            const now = new Date()
-                            const date = new Date(match.date)
-                            const nowPlaying = now.getTime() - date.getTime() > 0
-                            if (nowPlaying) {
-                              const a = match.home_team === item.id ? match.home_result : match.away_result
-                              const b = match.home_team === item.id ? match.away_result : match.home_result
-                              return <td key={timeString}>{a} – {b} {teams[competitor - 1].emojiString}</td>
-                            }
-                            const timeString = `${date.getHours()}:${getFullMinutes(date)}`
-                            const stadium = stadiums[match.stadium - 1]
-                            const place = localeCities[stadium.city]
-                            return (
-                              <td key={timeString} style={{ position: 'relative' }}>
-                                <p style={{ fontSize: '0.6em', color: 'rgb(170, 170, 170)', marginBottom: 0, position: 'absolute', top: 0, lineHeight: '1em' }}>{place}</p>
-                                {timeString} {teams[competitor - 1].emojiString}
-                              </td>
-                            )
-                          })}
-                        </tr>
-                      )
-                    }))}
-                    {index < array.length - 1 && (
-                      <tr>
-                        <td colSpan={6 + remainingMatches.length}></td>
-                      </tr>
-                    )}
-                  </tbody>
-                )
-              })}
-            </table>
+            <div className="knockout-wrapper">
+              <div className="knockout-column">
+                {firstRound16Matches.map(item => <KnockoutMatch key={item.name} match={item} />)}
+              </div>
+              <div className="knockout-column">
+                {firstRound8Matches.map(item => <KnockoutMatch key={item.name} match={item} />)}
+              </div>
+              <div className="knockout-column">
+                {firstRound4Matches.map(item => <KnockoutMatch key={item.name} match={item} />)}
+              </div>
+              <div className="knockout-column">
+                {knockout.round_2.matches.map(item => <FinalMatch key={item.name} match={item} />)}
+                {knockout.round_2_loser.matches.map(item => <FinalMatch semi key={item.name} match={item} />)}
+              </div>
+              <div className="knockout-column">
+                {secondRound4Matches.map(item => <KnockoutMatch key={item.name} match={item} />)}
+              </div>
+              <div className="knockout-column">
+                {secondRound8Matches.map(item => <KnockoutMatch key={item.name} match={item} />)}
+              </div>
+              <div className="knockout-column">
+                {secondRound16Matches.map(item => <KnockoutMatch key={item.name} match={item} />)}
+              </div>
+            </div>
           </section>
         </article>
       </Layout>
+    )
+  }
+
+  renderGroupTable = () => {
+    const { groups, groupMatches, stadiums, teams } = this.props
+    const remainingMatches = groupMatches.filter(item => new Date().getTime() <= Number(item[0]))
+
+    return (
+      <table className="table_nowrap">
+        <thead>
+          <tr>
+            <th colSpan={6} />
+            {remainingMatches.map((item) => {
+              const date = new Date(Number(item[0]))
+              const dateString = `${date.getDate()} ${localeMonthsGenitive[date.getMonth()]}`
+              return <th key={dateString}>{dateString}</th>
+            })}
+          </tr>
+        </thead>
+        {Object.entries(groups).map((group, index, array) => {
+          const groupTeams = this.getGroupTeams(group)
+          return (
+            <tbody key={group[0]}>
+              {groupTeams.map(((item, index) => {
+                const team = teams[item.id - 1]
+                return (
+                  <tr key={item.id}>
+                    <td>{index === 0 ? group[0].toUpperCase() : ''}</td>
+                    <td>{team.emojiString} {localeCountries[team.name]}</td>
+                    <td>{item.points}</td>
+                    {item.matches.map((match) => {
+                      if (!match.finished) {
+                        return <td key={match.name} />
+                      }
+                      const a = match.home_team === item.id ? match.home_result : match.away_result
+                      const b = match.home_team === item.id ? match.away_result : match.home_result
+                      const competitor = match.home_team === item.id ? match.away_team : match.home_team
+                      const backgroundColor = this.getBackgroundColor(a, b)
+                      return <td key={match.name} style={{ backgroundColor }}>{a} – {b} {teams[competitor - 1].emojiString}</td>
+                    })}
+                    {remainingMatches.map((remainingMatch) => {
+                      const match = item.matches.find(match => remainingMatch[1].map(item => item.name).includes(match.name))
+                      if (!match || match.finished) {
+                        return <td key={remainingMatch[0]} />
+                      }
+                      const competitor = match.home_team === item.id ? match.away_team : match.home_team
+                      const now = new Date()
+                      const date = new Date(match.date)
+                      const nowPlaying = now.getTime() - date.getTime() > 0
+                      if (nowPlaying) {
+                        const a = match.home_team === item.id ? match.home_result : match.away_result
+                        const b = match.home_team === item.id ? match.away_result : match.home_result
+                        return <td key={timeString}>{a} – {b} {teams[competitor - 1].emojiString}</td>
+                      }
+                      const timeString = `${date.getHours()}:${getFullMinutes(date)}`
+                      const stadium = stadiums[match.stadium - 1]
+                      const place = localeCities[stadium.city]
+                      return (
+                        <td key={timeString} style={{ position: 'relative' }}>
+                          <p style={{ fontSize: '0.6em', color: 'rgb(170, 170, 170)', marginBottom: 0, position: 'absolute', top: 0, lineHeight: '1em' }}>{place}</p>
+                          {timeString} {teams[competitor - 1].emojiString}
+                        </td>
+                      )
+                    })}
+                  </tr>
+                )
+              }))}
+              {index < array.length - 1 && (
+                <tr>
+                  <td colSpan={6 + remainingMatches.length}></td>
+                </tr>
+              )}
+            </tbody>
+          )
+        })}
+      </table>
     )
   }
 
