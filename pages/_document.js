e22de42d7c1b3ba99505bf4099c845cfc1faaba6
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-07-04 00:56:53 +0300

    Remove Chatra

--- pages/_document.js
+++ pages/_document.js
@@ -16,19 +16,6 @@ export default class MyDocument extends Document {
             gtag('js', new Date());
             gtag('config', 'UA-112751787-1');
           `}}></script>
-          <script dangerouslySetInnerHTML={{__html: `
-            (function(d, w, c) {
-                w.ChatraID = 'aeSjcHXJXuKpCPZPA';
-                var s = d.createElement('script');
-                w[c] = w[c] || function() {
-                    (w[c].q = w[c].q || []).push(arguments);
-                };
-                s.async = true;
-                s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
-                + '//call.chatra.io/chatra.js';
-                if (d.head) d.head.appendChild(s);
-            })(document, window, 'Chatra');
-          `}}></script>
         </Head>
         <body>
           <Main />
