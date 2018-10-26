8ca50afd372bcb67b6172bb8416f7e38b04f3a0e
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-10-27 02:03:35 +0300

    Update deps

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,20 @@
+import Head from 'next/head'
+
 import Header from './Header'
 import '../static/style.css'
 
 const Layout = (props) => [
+  <Head key="head">
+    <title>Саша Мансуров</title>
+    <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro:400,700&amp;subset=cyrillic" rel="stylesheet" />
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112751787-1"></script>
+    <script dangerouslySetInnerHTML={{__html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+      gtag('config', 'UA-112751787-1');
+    `}}></script>
+  </Head>,
   <Header key="header" />,
   <main key="main">{props.children}</main>
 ]
--- pages/_document.js
+++ pages/_document.js
@@ -5,17 +5,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>Александр Мансуров</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro:400,700&amp;subset=cyrillic" rel="stylesheet" />
-          <link rel="stylesheet" href="/_next/static/style.css" />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112751787-1"></script>
-          <script dangerouslySetInnerHTML={{__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-            gtag('config', 'UA-112751787-1');
-          `}}></script>
         </Head>
         <body>
           <Main />
